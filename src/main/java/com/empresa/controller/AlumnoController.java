2a50e7dc221c167aace1c44b8b9398d0223008b9
renzorevq <renzorevq@gmail.com>
2021-05-23 15:57:45 -0500

    metodo elimina

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -56,5 +58,17 @@ public class AlumnoController {
 		}else {
 			return ResponseEntity.badRequest().build();
 		}		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Alumno> elimina(@PathVariable("id")int idAlumno){
+		Optional<Alumno> optAlumno = service.obtienePorId(idAlumno);
+		if(optAlumno.isPresent()) {
+			service.eliminaAlumno(idAlumno);
+			return ResponseEntity.ok(optAlumno.get());
+		}
+		else {
+			return ResponseEntity.badRequest().build();
+		}
 	}
 }
